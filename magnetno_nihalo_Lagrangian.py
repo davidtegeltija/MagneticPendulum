2b052ffa3ae33ef67d274980a57a7a606d333fa6
David Tegeltija <david.tegeltija@gmail.com>
2022-05-06 11:19:27 +0200

    getting an error

--- magnetno_nihalo_Lagrangian.py
+++ magnetno_nihalo_Lagrangian.py
@@ -81,6 +81,9 @@ r_vector = smp.Matrix([x - x_magnet, y - y_magnet, z])
 r = r_vector.norm() #((x - x_magnet)**2 + (y - y_magnet)**2 + z**2)**0.5
 B_vector = mu_0/(4*smp.pi*r**5) * (3 * r_vector * m_vector_static.dot(r_vector) - r**2 * m_vector_static)
 
+
+print(m_vector_dinamic.dot(B_vector))
+
 #Kinetic energy
 T = 1/2 * M * (smp.diff(x, t)**2 + smp.diff(y, t)**2 + smp.diff(z, t)**2)
 
@@ -92,9 +95,11 @@ L = T - V
 
 EL1 = smp.diff(L, theta) - smp.diff(smp.diff(L, theta_d), t).simplify()
 EL2 = smp.diff(L, phi) - smp.diff(smp.diff(L, phi_d), t).simplify()
+print("\n", EL1, "\n")
+print(EL2)
 
 equations = smp.solve([EL1, EL2], (theta_dd, phi_dd), simplify=False, rational=False)
-
+print(equations[theta_dd])
 dtheta_dt_fun = smp.lambdify(theta_d, theta_d)
 dphi_dt_fun = smp.lambdify(phi_d, phi_d)
 omega_theta_fun = smp.lambdify((t, g, l, M, mu_0, magnetic_moment, x_magnet, y_magnet, theta, phi, theta_d, phi_d) , equations[theta_dd])
@@ -129,12 +134,12 @@ omega = np.sqrt(g/l)
 theta = np.pi/2
 phi = 0
 dtheta_dt = 0.
-dphi_dt = 1
+dphi_dt = 0.8
 
 initial_conditions = [theta, phi, dtheta_dt, dphi_dt]
 
 dt = 0.01
-t = np.arange(0.0, 5, dt)
+t = np.arange(0.0, 10, dt)
 
 magnets=[0.2, 0.2]
 # magnets = configuration_of_magnets(x, y, 6, 0.8, False)
@@ -219,100 +224,100 @@ plt.show()
 
 
 
-############################ CONSTANTS and FUNCTIONS ############################
-mu_0 = 4*np.pi * 1e-7
-g = 9.81
-L = 1. #visina magneta na kateri visi
-R = 0.1 #radij magneta
-omega = np.sqrt(g/L)
+# ############################ CONSTANTS and FUNCTIONS ############################
+# mu_0 = 4*np.pi * 1e-7
+# g = 9.81
+# L = 1. #visina magneta na kateri visi
+# R = 0.1 #radij magneta
+# omega = np.sqrt(g/L)
 
 
-# N = 100
-# x = np.linspace(-50.0, 50.0, N)
-# y = np.linspace(-50.0, 50.0, N)
-# z = np.linspace(-10.0, 10.0, N)
-dx = 0.01
-dy = 0.01
-border = L
-x = np.arange(-border, border + dx, dx)
-y = np.arange(-border, border + dy, dy)
+# # N = 100
+# # x = np.linspace(-50.0, 50.0, N)
+# # y = np.linspace(-50.0, 50.0, N)
+# # z = np.linspace(-10.0, 10.0, N)
+# dx = 0.01
+# dy = 0.01
+# border = L
+# x = np.arange(-border, border + dx, dx)
+# y = np.arange(-border, border + dy, dy)
 
 
-def B(r, m):
-    return mu_0/4*np.pi * (3. * r * np.dot(m, r)/np.linalg.norm(r)**5 - m/np.linalg.norm(r)**3)
+# def B(r, m):
+#     return mu_0/4*np.pi * (3. * r * np.dot(m, r)/np.linalg.norm(r)**5 - m/np.linalg.norm(r)**3)
 
-def energy_of_dipole_in_B(theta, phi, static_m):
-    x = np.sin(theta) * np.cos(phi)
-    y = np.sin(theta) * np.sin(phi)
-    z = 1 - np.cos(theta)
-    r_vec = np.array([x, y, z])
-    mag_field = B(r_vec, static_m)
-
-
-
-def second_order_theta(theta, phi, dthetadt, dphidt, magnets):
-    return np.sin(theta) * np.cos(theta) * dphidt **2 - omega**2 * np.sin(theta)
-
-def second_order_phi(theta, phi, dthetadt, dphidt, magnets):
-    return -2 * dthetadt * dphidt * np.cos(theta)/np.sin(theta)
+# def energy_of_dipole_in_B(theta, phi, static_m):
+#     x = np.sin(theta) * np.cos(phi)
+#     y = np.sin(theta) * np.sin(phi)
+#     z = 1 - np.cos(theta)
+#     r_vec = np.array([x, y, z])
+#     mag_field = B(r_vec, static_m)
 
 
 
-# a simple pendulum y''= F(y) , state = (y,v)
-def pendulum(state, t, magnets): 
-    theta, phi, dtheta_dt, dphi_dt = state
-    derivitives = np.zeros_like(state)
-    derivitives[0] = dtheta_dt                #dxdt[0] = derivites[0] = state[1] = v_x
-    derivitives[1] = dphi_dt                #dydt[0] = derivites[1] = state[2] = v_y
-    # derivitives[2] = v_z
-    derivitives[2] = second_order_theta(theta, phi, dtheta_dt, dphi_dt, magnets)     #dxdt[1] = derivites[2] = F_x
-    derivitives[3] = second_order_phi(theta, phi, dtheta_dt, dphi_dt, magnets)     #dydt[1] = derivites[3] = F_y
-    # derivitives[5] = force(x, y, z, magnets)[2]
-    return derivitives
+# def second_order_theta(theta, phi, dthetadt, dphidt, magnets):
+#     return np.sin(theta) * np.cos(theta) * dphidt **2 - omega**2 * np.sin(theta)
+
+# def second_order_phi(theta, phi, dthetadt, dphidt, magnets):
+#     return -2 * dthetadt * dphidt * np.cos(theta)/np.sin(theta)
 
 
 
-############################ SOLUTION ############################
-theta = np.pi/3
-phi = np.pi/2
-dtheta_dt = 0.
-dphi_dt = 0.5
-
-initial_conditions = [theta, phi, dtheta_dt, dphi_dt]
-
-dt = 0.01
-t = np.arange(0.0, 5, dt)
-
-
-magnets = configuration_of_magnets(x, y, 6, 0.8, False)
-for i in [ [0.2, 0.2], [0.4, 0.1], [-0.1, 0.6] ]:
-    magnets = np.append(magnets, i)
-magnets = magnets.reshape([9, 2])
-
-solution = odeint(pendulum, initial_conditions, t, args=(magnets,))
+# # a simple pendulum y''= F(y) , state = (y,v)
+# def pendulum(state, t, magnets): 
+#     theta, phi, dtheta_dt, dphi_dt = state
+#     derivitives = np.zeros_like(state)
+#     derivitives[0] = dtheta_dt                #dxdt[0] = derivites[0] = state[1] = v_x
+#     derivitives[1] = dphi_dt                #dydt[0] = derivites[1] = state[2] = v_y
+#     # derivitives[2] = v_z
+#     derivitives[2] = second_order_theta(theta, phi, dtheta_dt, dphi_dt, magnets)     #dxdt[1] = derivites[2] = F_x
+#     derivitives[3] = second_order_phi(theta, phi, dtheta_dt, dphi_dt, magnets)     #dydt[1] = derivites[3] = F_y
+#     # derivitives[5] = force(x, y, z, magnets)[2]
+#     return derivitives
 
 
 
+# ############################ SOLUTION ############################
+# theta = np.pi/3
+# phi = np.pi/2
+# dtheta_dt = 0.
+# dphi_dt = 0.5
 
-plt.plot(magnets[:, 0], magnets[:, 1], "o")
-plt.grid()
-plt.show()
+# initial_conditions = [theta, phi, dtheta_dt, dphi_dt]
 
-plt.plot(t, solution[:, 0], label="x") #resitev za x
-plt.plot(t, solution[:, 1], label="y") #resitev za y
-plt.plot(t, solution[:, 2])
-plt.legend()
-plt.show()
+# dt = 0.01
+# t = np.arange(0.0, 5, dt)
 
-for i in [0, 10, 20, 50, 100, 120, 150, 175, 200]:
-    plt.plot(solution[i, 0], solution[i, 1], "o", label="{}s".format(i))
-plt.plot(solution[175, 0], solution[175, 1], "o")
-plt.plot(magnets[:, 0], magnets[:, 1], "o", markersize=10, color="black", label="magnets")
-# plt.xlim(-border - 1, border + 1)
-# plt.ylim(-border - 1, border + 1)
-plt.grid()
-plt.legend()
-plt.show()
+
+# magnets = configuration_of_magnets(x, y, 6, 0.8, False)
+# for i in [ [0.2, 0.2], [0.4, 0.1], [-0.1, 0.6] ]:
+#     magnets = np.append(magnets, i)
+# magnets = magnets.reshape([9, 2])
+
+# solution = odeint(pendulum, initial_conditions, t, args=(magnets,))
+
+
+
+
+# plt.plot(magnets[:, 0], magnets[:, 1], "o")
+# plt.grid()
+# plt.show()
+
+# plt.plot(t, solution[:, 0], label="x") #resitev za x
+# plt.plot(t, solution[:, 1], label="y") #resitev za y
+# plt.plot(t, solution[:, 2])
+# plt.legend()
+# plt.show()
+
+# for i in [0, 10, 20, 50, 100, 120, 150, 175, 200]:
+#     plt.plot(solution[i, 0], solution[i, 1], "o", label="{}s".format(i))
+# plt.plot(solution[175, 0], solution[175, 1], "o")
+# plt.plot(magnets[:, 0], magnets[:, 1], "o", markersize=10, color="black", label="magnets")
+# # plt.xlim(-border - 1, border + 1)
+# # plt.ylim(-border - 1, border + 1)
+# plt.grid()
+# plt.legend()
+# plt.show()
 
 
 
